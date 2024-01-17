9b31f2b67541980a87539f11815e097bfcedda72
ishanjarwal <ishan.professional01@gmail.com>
2024-01-17 15:51:56 +0530

    multiple actions by switch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// use of logger middleware
+// multiple actions by switch case
 
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
@@ -9,14 +9,23 @@ function reducer(state = { amount: 0 }, action) {
     switch (action.type) {
         case 'increment':
             return { amount: state.amount + 1 }
+        case 'decrement':
+            return { amount: state.amount - 1 }
+        case 'incrementByAmount':
+            return { amount: state.amount + action.payload }
+        case 'decrementByAmount':
+            return { amount: state.amount + action.payload }
         default:
             return state;
     }
 }
 
 
+const decrease = { type: 'decrement' }
 const increase = { type: 'increment' }
+const increaseByAmount = { type: 'incrementByAmount', payload: 100 }
+const decreaseByAmount = { type: 'decrementByAmount', payload: 100 }
 
 setInterval(() => {
-    store.dispatch(increase);
-}, 2000);
+    store.dispatch(increaseByAmount);
+}, 3000);
