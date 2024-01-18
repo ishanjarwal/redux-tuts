faa8d3b7f8b88da043cffa2c029da3689c396bad
ishanjarwal <ishan.professional01@gmail.com>
2024-01-18 11:33:14 +0530

    multiple-reducers

--- index.js
+++ index.js
@@ -1,22 +1,65 @@
-// use of logger middleware
+// multiple reducers
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import logger from 'redux-logger'
+import axios from 'axios'
+import { thunk } from 'redux-thunk'
 
-const store = createStore(reducer, applyMiddleware(logger.default));
+const init = 'accounts/init';
+const increment = 'accounts/increment';
+const decrement = 'accounts/decrement';
+const incrementByAmount = 'accounts/incrementByAmount';
+const decrementByAmount = 'accounts/decrementByAmount';
 
-function reducer(state = { amount: 0 }, action) {
+const incrementInBonus = 'bonus/incrementInBonus';
+
+
+const store = createStore(combineReducers({
+    accounts: accountReducer,
+    bonus: bonusReducer
+}), applyMiddleware(logger.default, thunk));
+
+
+// reducer 1
+function accountReducer(state = { amount: 0 }, action) {
     switch (action.type) {
-        case 'increment':
+        case init:
+            return { amount: action.payload }
+        case increment:
             return { amount: state.amount + 1 }
+        case decrement:
+            return { amount: state.amount - 1 }
+        case incrementByAmount:
+            return { amount: state.amount + action.payload }
+        case decrementByAmount:
+            return { amount: state.amount + action.payload }
         default:
             return state;
     }
 }
 
+// reducer 2
+function bonusReducer(state = { points: 0 }, action) {
+    switch (action.type) {
+        case incrementInBonus:
+            return { points: state.points + 1 }
+        case incrementByAmount:
+            if (action.payload >= 100)
+                return { points: state.points + 1 }
+        default:
+            return state
+    }
+}
 
-const increase = { type: 'increment' }
 
-setInterval(() => {
-    store.dispatch(increase);
-}, 2000);
+// action creators
+function decrease() { return { type: decrement } }
+function increase() { return { type: increment } }
+function increaseByAmount(payload) { return { type: incrementByAmount, payload: payload } }
+function decreaseByAmount(payload) { return { type: decrementByAmount, payload: payload } }
+function increaseBonus() { return { type: incrementInBonus } }
+
+// affects both reducers' state because action is same
+store.dispatch(increaseBonus());
+
+
